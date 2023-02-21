eef445415e281f813283c7c488d8c095f8016c7b
kundan571 <kundanchauhan1997@gmail.com>
2023-02-22 01:04:45 +0530

    Added: simple linkedlist

--- src/com/linkedlistoperations/LinkedList.java
+++ src/com/linkedlistoperations/LinkedList.java
@@ -1,7 +1,33 @@
 package com.linkedlistoperations;
 
+import org.w3c.dom.Node;
+
 public class LinkedList {
+    Node head;
+    class Node{
+        String inputNode;
+        Node next;
+        Node(String inputNode){
+            this.inputNode = inputNode;
+            this.next = null;
+        }
+
+    }
+    // Add method
+    public void addFirst(String inputNode){
+        Node newNode = new Node(inputNode);
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to LinkedList Program");
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst("70");
+        linkedList.addFirst("30");
+        linkedList.addFirst("56");
+
     }
 }
